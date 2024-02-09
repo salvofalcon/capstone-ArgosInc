5425eb8b827a0542ef3c691fca895e7e58313970
Chandler <ctn4@students.uwf.edu>
2024-02-09 11:49:47 -0600

    Adding appshell + dark mode

--- nutribuddy/src/App.tsx
+++ nutribuddy/src/App.tsx
@@ -1,12 +1,32 @@
 import "./App.css";
 import { HeaderMegaMenu } from "./components/HeaderMegaMenu/HeaderMegaMenu";
 import { HeroTitle } from "./components/HeroTitle/HeroTitle";
+import { AppShell } from "@mantine/core";
 
 function App() {
   return (
     <>
-      <HeaderMegaMenu />
-      <HeroTitle />
+      <AppShell
+        header={{ height: 60 }}
+        /*
+        navbar={{
+          width: 300,
+          breakpoint: "sm",
+          //collapsed: { mobile: !opened },
+        }}
+        */
+        padding="md"
+      >
+        <AppShell.Header>
+          <HeaderMegaMenu />
+        </AppShell.Header>
+
+        {/*<AppShell.Navbar p="md">Navbar</AppShell.Navbar>*/}
+
+        <AppShell.Main>
+          <HeroTitle />
+        </AppShell.Main>
+      </AppShell>
     </>
   );
 }
--- nutribuddy/src/main.tsx
+++ nutribuddy/src/main.tsx
@@ -10,7 +10,7 @@ const theme = createTheme({
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <MantineProvider theme={theme}>
+  <MantineProvider theme={theme} defaultColorScheme="dark">
     <React.StrictMode>
       <App />
     </React.StrictMode>

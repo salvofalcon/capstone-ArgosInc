352ad977ab1c960acd1588b3bf0f716a42c197ac
Chandler <ctn4@students.uwf.edu>
2024-04-17 18:22:45 -0500

    Adding total calories logged

--- nutribuddy/src/components/Diary/Diary.tsx
+++ nutribuddy/src/components/Diary/Diary.tsx
@@ -1,5 +1,5 @@
 import React, { Component, ReactNode } from "react";
-import { Button, Center, Paper, Stack } from "@mantine/core";
+import { Button, Center, Container, Paper, Stack } from "@mantine/core";
 import { DatePicker } from "../DatePicker/DatePicker";
 import { rem } from "@mantine/core";
 import { IconCalendar } from "@tabler/icons-react";
@@ -8,13 +8,14 @@ import "@mantine/dates/styles.css";
 
 export default class Diary extends Component<
   object,
-  { userData: any; selectedDate: any }
+  { userData: any; selectedDate: any; calories: number }
 > {
   constructor(props: any) {
     super(props);
     this.state = {
       userData: "",
       selectedDate: new Date(),
+      calories: 0,
     };
   }
   componentDidMount(): void {
@@ -45,54 +46,6 @@ export default class Diary extends Component<
     window.localStorage.clear();
     window.location.href = "./login";
   };
-
-  //   renderEntries = () => {
-  //     if (this.state.userData.entries && this.state.userData.entries.length > 0) {
-  //       for (let i = 0; i < this.state.userData.entries.length; i++) {
-  //         // Extract foodName from the first entry
-  //         const foodName = this.state.userData.entries[i].foodName;
-  //         const dateNeeded = this.state.userData.entries[i].date;
-  //         //Transform the date to match selected date
-  //         const dateSelected = new Date(this.state.selectedDate);
-  //         const neededDate = new Date(this.state.userData.entries[i].date);
-  //         const dateSelectedTransformed = new Date(
-  //           dateSelected.getFullYear(),
-  //           dateSelected.getMonth(),
-  //           dateSelected.getDate()
-  //         );
-
-  //         const dateNeededTransformed = new Date(
-  //           neededDate.getFullYear(),
-  //           neededDate.getMonth(),
-  //           neededDate.getDate()
-  //         );
-  //         console.log("DATE SELECTED: " + dateSelectedTransformed);
-  //         console.log("DATE NEEDED: " + dateNeededTransformed);
-  //         //NEED TO LOOP THROUGH ALL ENTRIES TO COMPARE DATE AND RENDER ON MATCH
-  //         //ALSO NEED TO EVENTUALLY SORT BY BREAKFAST, LUNCH, DINNER, SNACKS
-  //         //WILL ALSO NEED TO DISPLAY CALORIES/MACROS
-  //         //AND ADD A BUTTON TO SEARCH FOR MORE FOOD
-  //         if (
-  //           dateSelectedTransformed.getMonth() ==
-  //             dateNeededTransformed.getMonth() &&
-  //           dateSelectedTransformed.getFullYear() ==
-  //             dateNeededTransformed.getFullYear() &&
-  //           dateSelectedTransformed.getDate() == dateNeededTransformed.getDate()
-  //         ) {
-  //           console.log("THEY ARE EQUAL");
-  //           return (
-  //             <>
-  //               Food Name<h1>{foodName}</h1>
-  //               Date Needed<h1>{dateNeeded}</h1>
-  //             </>
-  //           );
-  //         }
-  //       }
-  //       return <>No entries for this day</>;
-  //     } else {
-  //       return <h1>No entries found</h1>;
-  //     }
-  //   };
 
   renderBreakfastEntries = () => {
     const { userData, selectedDate } = this.state;
@@ -133,7 +86,6 @@ export default class Diary extends Component<
             <div key={index}>
               <h3>{entry.foodName}</h3>
               <p>Calories: {entry.calories.toFixed()}</p>
-              {/* Render other fields of the entry here */}
             </div>
           )
         );
@@ -184,12 +136,111 @@ export default class Diary extends Component<
             <div key={index}>
               <h3>Food Name: {entry.foodName}</h3>
               <p>Calories: {entry.calories.toFixed()}</p>
-              {/* Render other fields of the entry here */}
             </div>
           )
         );
       } else {
         return <p>No lunch entries found for the selected date</p>;
+      }
+    } else {
+      return <h2>No entries found</h2>;
+    }
+  };
+
+  renderDinnerEntries = () => {
+    const { userData, selectedDate } = this.state;
+    if (userData.entries && userData.entries.length > 0) {
+      const entriesForSelectedDate = userData.entries.filter(
+        (entry: { mealType: string; date: string | number | Date }) => {
+          const entryDate = new Date(entry.date);
+          return (
+            entryDate.getFullYear() === selectedDate.getFullYear() &&
+            entryDate.getMonth() === selectedDate.getMonth() &&
+            entryDate.getDate() === selectedDate.getDate() &&
+            entry.mealType === "Dinner"
+          );
+        }
+      );
+
+      if (entriesForSelectedDate.length > 0) {
+        return entriesForSelectedDate.map(
+          (
+            entry: {
+              calories: number;
+              foodName: string | number | null | undefined;
+              date:
+                | string
+                | number
+                | boolean
+                | React.ReactElement<
+                    any,
+                    string | React.JSXElementConstructor<any>
+                  >
+                | Iterable<React.ReactNode>
+                | React.ReactPortal
+                | null
+                | undefined;
+            },
+            index: React.Key | null | undefined
+          ) => (
+            <div key={index}>
+              <h3>Food Name: {entry.foodName}</h3>
+              <p>Calories: {entry.calories.toFixed()}</p>
+            </div>
+          )
+        );
+      } else {
+        return <p>No dinner entries found for the selected date</p>;
+      }
+    } else {
+      return <h2>No entries found</h2>;
+    }
+  };
+
+  renderSnackEntries = () => {
+    const { userData, selectedDate } = this.state;
+    if (userData.entries && userData.entries.length > 0) {
+      const entriesForSelectedDate = userData.entries.filter(
+        (entry: { mealType: string; date: string | number | Date }) => {
+          const entryDate = new Date(entry.date);
+          return (
+            entryDate.getFullYear() === selectedDate.getFullYear() &&
+            entryDate.getMonth() === selectedDate.getMonth() &&
+            entryDate.getDate() === selectedDate.getDate() &&
+            entry.mealType === "Snacks"
+          );
+        }
+      );
+
+      if (entriesForSelectedDate.length > 0) {
+        return entriesForSelectedDate.map(
+          (
+            entry: {
+              calories: number;
+              foodName: string | number | null | undefined;
+              date:
+                | string
+                | number
+                | boolean
+                | React.ReactElement<
+                    any,
+                    string | React.JSXElementConstructor<any>
+                  >
+                | Iterable<React.ReactNode>
+                | React.ReactPortal
+                | null
+                | undefined;
+            },
+            index: React.Key | null | undefined
+          ) => (
+            <div key={index}>
+              <h3>Food Name: {entry.foodName}</h3>
+              <p>Calories: {entry.calories.toFixed()}</p>
+            </div>
+          )
+        );
+      } else {
+        return <p>No snack entries found for the selected date</p>;
       }
     } else {
       return <h2>No entries found</h2>;
@@ -205,9 +256,29 @@ export default class Diary extends Component<
     const icon = (
       <IconCalendar style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
     );
+
+    let totalCalories = 0;
+    if (this.state.userData.entries && this.state.userData.entries.length > 0) {
+      const { selectedDate } = this.state;
+      const entriesForSelectedDate = this.state.userData.entries.filter(
+        (entry: { date: string | number | Date }) => {
+          const entryDate = new Date(entry.date);
+          return (
+            entryDate.getFullYear() === selectedDate.getFullYear() &&
+            entryDate.getMonth() === selectedDate.getMonth() &&
+            entryDate.getDate() === selectedDate.getDate()
+          );
+        }
+      );
+
+      totalCalories = entriesForSelectedDate.reduce(
+        (acc: number, entry: { calories: number }) => acc + entry.calories,
+        0
+      );
+    }
     return (
       <>
-        <Paper shadow="sm" radius="md" p="lg">
+        <Container>
           <Center>
             <h1>Your Food Diary</h1>
           </Center>
@@ -219,9 +290,11 @@ export default class Diary extends Component<
             value={this.state.selectedDate}
             onChange={this.handleDateChange}
           />
+          <h2>Calories Consumed: {totalCalories.toFixed()}</h2>
           <Stack>
             <Paper withBorder p="lg" mt="lg">
               <h2>Breakfast</h2>
+              <hr></hr>
               {this.renderBreakfastEntries()}
               <Button color="green" variant="transparent" radius="lg">
                 Add Food
@@ -230,6 +303,7 @@ export default class Diary extends Component<
 
             <Paper withBorder p="lg" mt="lg">
               <h2>Lunch</h2>
+              <hr></hr>
               {this.renderLunchEntries()}
               <Button color="green" variant="transparent" radius="lg">
                 Add Food
@@ -238,7 +312,8 @@ export default class Diary extends Component<
 
             <Paper withBorder p="lg" mt="lg">
               <h2>Dinner</h2>
-              {this.renderLunchEntries()}
+              <hr></hr>
+              {this.renderDinnerEntries()}
               <Button color="green" variant="transparent" radius="lg">
                 Add Food
               </Button>
@@ -246,7 +321,8 @@ export default class Diary extends Component<
 
             <Paper withBorder p="lg" mt="lg">
               <h2>Snacks</h2>
-              {this.renderLunchEntries()}
+              <hr></hr>
+              {this.renderSnackEntries()}
               <Button color="green" variant="transparent" radius="lg">
                 Add Food
               </Button>
@@ -261,7 +337,7 @@ export default class Diary extends Component<
               Sign out
             </Button>
           </Center>
-        </Paper>
+        </Container>
       </>
     );
   }
